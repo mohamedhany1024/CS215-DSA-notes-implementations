85c9b66428b777266e4c2be3d3c70015486644ce
Mohamed Hany <mhany4485@gmail.com>
2026-04-18 16:15:12 +0200

    added merge_sort and count_sort to sort.cpp

--- code/sort.cpp
+++ code/sort.cpp
@@ -152,17 +152,100 @@ void quick_sort(int l, int r, vector<int>& v) {
     }
 }
 
-vector<int> merge()
+void merge(int l, int r, vector<int>& a) {
+    int mid = (l+r)/2;
+    int i=l, j=mid+1;
+    vector<int> b;
+    b.reserve((r-l)+1);
+   
+    while(i<=mid && j<=r) {
+        if (a[i]<=a[j]) {
+            b.push_back(a[i]);
+            i++;
+            continue;
+        } 
+
+        if (a[j] <= a[i]) {
+            b.push_back(a[j]);
+            j++;
+            continue;
+        }
+    }
+
+    while(i<=mid) {
+        b.push_back(a[i]);
+        i++;
+    }
+
+    while (j<=r) {
+        b.push_back(a[j]);
+        j++;
+    }
+
+    i=0;
+    int k = 0;
+    for(int k=l; k<=r; k++) {
+        a[k] = b[i];
+        i++;
+    }
+}
+
+void merge_sort(int l, int r, vector<int>& a) {
+    if (l>=r) {
+        return;
+    } else if (r-l == 1)
+    {
+        if (a[l] > a[r])
+        {
+            swap(a[l], a[r]);
+        }
+        return;
+    }
+
+    int mid = (l+r) / 2;
+    merge_sort(l, mid, a);
+    merge_sort(mid+1, r, a);
+    merge(l, r, a);
+}
+
+
+
+vector <int> count_sort(vector<int> A) {
+   
+    vector<int> B(A.size());
+    int maxNum = A[0];
+    for(int i=1; i<B.size(); i++) {
+        maxNum = max(maxNum, A[i]);
+    }
+
+    vector<int> C(maxNum+1, 0);
+   
+    for(int i=0; i<A.size(); i++) {
+        C[A[i]]++;
+    }
+    for(int i=1; i<C.size(); i++) {
+        C[i] = C[i] + C[i-1];
+    }
+    for(int i=A.size()-1; i>=0; i--) {
+        // we used B[C[A[i]]-1] because of zero-based indexing
+        B[C[A[i]]-1] = A[i];
+        C[A[i]]--;
+    }
+
+    return B;
+
+}
 
 int main() {
 
     vector <int> v1 = {5, 3, 2, 7, 4, 25, 1, 16};
     vector <int> v2= {15, 31, 52, 64, 17, 43, 27, 19, 68, 41, 63, 2, 78, 14, 12, 67, 7, 98, 54, 39, 59, 73, 79, 87, 61, 36, 74, 66, 44, 92, 38, 34, 6, 83, 51, 57, 69, 33, 13, 49, 4, 90, 99, 1, 18, 89, 42, 97, 82, 40, 46, 48, 93, 62, 50, 53, 29, 8, 30, 94, 9, 5, 100, 25, 72, 24, 45, 86, 26, 35, 32, 37, 11, 60, 55, 80, 76, 77, 81, 47, 95, 58, 85, 10, 28, 65, 16, 20, 91, 75, 70, 56, 84, 3, 88, 96, 71, 21, 22, 23};
-    quick_sort(0, v2.size()-1, v2);
+    merge_sort(0, v2.size()-1, v2);
+    //v2 = count_sort(v2);
     for (auto& x : v2) {
         cout << x <<" "; 
     }
-    cout << endl;
-    cout << isSorted(v2);
+    cout  <<  endl;
+    cout << isSorted(v2) << endl;
     return 0;
 }
